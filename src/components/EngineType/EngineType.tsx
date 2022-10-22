14b3d9da47b8678bedfbe5a24b1fe0345c21f9be
Zayac11 <brunogrbstudios@gmail.com>
2022-10-22 22:14:55 +0300

    Added last graphic

--- src/components/EngineType/EngineType.tsx
+++ src/components/EngineType/EngineType.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './EngineType.module.scss'
 import Title from '../Title/Title'
+import Graph from '../Graph/Graph'
 
 const EngineType = () => {
     return (
         <div className={styles['engine']}>
             <div className={styles['title']}>
                 <Title>История по типу двигателя</Title>
+            </div>
+            <div className={styles['content']}>
+                <Graph xAxes={'name'} yAxes={'uv'} yLabel={'Вэлью'} xLabel={'Время'} color={'blue'} type={'Bar'} />
             </div>
         </div>
     )
